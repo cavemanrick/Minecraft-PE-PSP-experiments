62c3d3f8d3d909a25beca671778dfc6460d4fa2b
cavemanrick <cavemanrick1226@yahoo.com>
2026-07-23 17:03:30 -0400

    Add B_JUNGLE to BiomeId enum

--- src/world/level/levelgen/biome.h
+++ src/world/level/levelgen/biome.h
@@ -1,8 +1,9 @@
+src/world/level/levelgen/biome.h
 #ifndef BIOME_H__
 #define BIOME_H__
 
 enum BiomeId { B_TUNDRA, B_SAVANNA, B_DESERT, B_SWAMP, B_TAIGA, B_SHRUB,
-               B_FOREST, B_PLAINS, B_SEASONAL, B_RAIN };
+               B_FOREST, B_PLAINS, B_SEASONAL, B_RAIN, B_JUNGLE };
 
 BiomeId classifyBiome(float temperature, float downfall);
 void biomeSurface(BiomeId b, unsigned char* top, unsigned char* material);
